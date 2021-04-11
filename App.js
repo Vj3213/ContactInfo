c6276d9f758a8f84befc46f4db3a2bf3e41f1db7
Ved Jha <vedprakash@sportzinteractive.net>
2021-04-12 00:08:42 +0530

    finished react-navigation setup

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Home from './screens/Home';
 import store from './store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Home />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     )
   }
